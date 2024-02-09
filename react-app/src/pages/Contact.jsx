acb3095987a4be906565aeaa6e71328a56f6ac76
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2024-02-09 09:31:32 +0900

    change phone

--- react-app/src/pages/Contact.jsx
+++ react-app/src/pages/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div className='pl-10 bg-base-100 flex flex-col items-center'>
             <h1 className="mb-5 text-5xl font-bold">Contact</h1>
-            <li>tel:070-3602-2836</li>
+            <li>tel: 236-889-1029</li>
             <li>email: aries0326business@gmail.com</li>
         </div>
     );
